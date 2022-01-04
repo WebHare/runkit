4711cbb09a53dbd21f5b3b44a0cc830a8a206ec8
Rob Hulswit <rob@webhare.nl>
2022-01-04 11:38:08 +0100

    Add script to list available backups

---
+++ bin/list-backups.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e #fail on any uncaught error
+
+exit_syntax()
+{
+  echo "Syntax: list-backups.sh"
+  exit 1
+}
+
+source "${BASH_SOURCE%/*}/../libexec/functions.sh"
+
+while true; do
+  if [ "$1" == "--help" ]; then
+    exit_syntax
+  elif [[ "$1" =~ ^-.* ]]; then
+    echo "Invalid switch '$1'"
+    exit 1
+  else
+    break
+  fi
+done
+
+CONTAINER="$1"
+ensurecommands ssh-add borg
+applyborgsettings "$CONTAINER"
+
+exec borg list
