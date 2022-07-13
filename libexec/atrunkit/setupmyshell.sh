9cfda977f1da8316af68fd47ab417bcc6002f0fa
Arnold Hendriks <arnold@webhare.nl>
2022-07-13 23:07:58 +0200

    setup wh- aliases

--- libexec/atrunkit/setupmyshell.sh
+++ libexec/atrunkit/setupmyshell.sh
@@ -4,6 +4,14 @@ if [ -t 1 ]; then
   exit 1
 fi
 
+# TODO unregister wh- aliases for servers since removed? but this may for now be annoying for users who manually set up wh-xxx aliasses..
+
+for SERVER in $( cd "$WHRUNKIT_ROOT/local" ||exit 1; echo * ); do
+  if [ -f "$WHRUNKIT_ROOT/local/$SERVER/dataroot" ]; then # it appears to be a usable insatllation...
+    echo "wh-$SERVER() { \"$WHRUNKIT_ORIGCOMMAND\" \"@$SERVER\" wh \"\$@\" ; } ;"
+  fi
+done
+
 cat << HERE
 
 runkit() { "$WHRUNKIT_ORIGCOMMAND" "\$@"; } ;
