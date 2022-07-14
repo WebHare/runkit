2be3df3016c4dc3a670e7c34279c200cfcaf8e22
Arnold Hendriks <arnold@webhare.nl>
2022-07-14 17:03:18 +0200

    close opensearch dashboard after terminating script

--- libexec/atserver/opensearch-dashboard.sh
+++ libexec/atserver/opensearch-dashboard.sh
@@ -6,4 +6,17 @@ if [ ! -x /usr/local/opt/opensearch-dashboards/bin/opensearch-dashboards ]; then
 fi
 
 /usr/local/opt/opensearch-dashboards/bin/opensearch-dashboards --opensearch.hosts="http://127.0.0.1:$((WEBHARE_BASEPORT + 6))/" --port="$((WEBHARE_BASEPORT + 7))" &
-open "http://127.0.0.1:$((WEBHARE_BASEPORT + 7))/"
+
+trap "kill %1; wait %1" TERM EXIT
+
+OPENURL="http://127.0.0.1:$((WEBHARE_BASEPORT + 7))/"
+
+while true; do
+  if curl --silent --fail "$OPENURL" >/dev/null 2>&1 ; then
+    break # we have a connection
+  fi
+  sleep .3
+done
+
+open "$OPENURL"
+wait %1
