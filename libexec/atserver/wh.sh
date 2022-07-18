9a1e11c74b22b64ad288112dd49d59008086453f
Rob Hulswit <rob@webhare.nl>
2022-07-18 14:59:00 +0200

    Allow setting WEBHARE_DIR in environment.sh, using it for executing wh

--- libexec/atserver/wh.sh
+++ libexec/atserver/wh.sh
@@ -2,6 +2,11 @@
 
 # FIXME how to find the source/code root ?
 
+if [ -n "$WEBHARE_DIR" ] && [ -x "$WEBHARE_DIR/bin/wh" ]; then
+  exec "$WEBHARE_DIR/bin/wh" "$@"
+  exit 255
+fi
+
 if [ -x ~/projects/webhare/whtree/bin/wh ]; then
   exec ~/projects/webhare/whtree/bin/wh "$@"
   exit 255
