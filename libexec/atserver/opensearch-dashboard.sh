a9704bbe251fa357cc95309dfb0ed759d3227a44
Arnold Hendriks <arnold@webhare.nl>
2022-10-27 13:55:34 +0200

    Allow setting the host to opensearch-dashboard

--- libexec/atserver/opensearch-dashboard.sh
+++ libexec/atserver/opensearch-dashboard.sh
@@ -1,16 +1,50 @@
 #!/bin/bash
 
+# syntax: [--host bind to host] [--port port]
+# short: Start OpenSearch dashboard for your local WebHare installation
+
+function exit_syntax
+{
+  echo "Syntax: runkit create-server [--primary] [--baseport <port>] <server>"
+  echo "        --primary  sets the baseport to 13679 and binds the server to the 'wh' alias"
+  echo "        <server>   short name for the server, used as wh-<server> alias"
+  echo "        <datadir>  where your data is currently stored (eg ~/projects/whdata/myserver/)"
+  exit 1
+}
+
+DASHBOARD_HOST="127.0.0.1"
+DASHBOARD_PORT="$((WEBHARE_BASEPORT + 7))"
+
+while true; do
+  if [ "$1" == "--host" ]; then
+    shift
+    DASHBOARD_HOST="$1"
+    shift
+  elif [ "$1" == "--port" ]; then
+    shift
+    DASHBOARD_PORT="$1"
+    shift
+  elif [ "$1" == "--help" ]; then
+    exit_syntax
+  elif [[ "$1" =~ ^-.* ]]; then
+    echo "Invalid switch '$1'"
+    exit 1
+  else
+    break
+  fi
+done
+
 if [ ! -x /usr/local/opt/opensearch-dashboards/bin/opensearch-dashboards ]; then
   echo "You need to brew install opensearch-dashboards" 1>&2
   exit 1
 fi
 
 [ -n "$WEBHARE_OPENSEARCH_BINDHOST" ] || WEBHARE_OPENSEARCH_BINDHOST="127.0.0.1"
-/usr/local/opt/opensearch-dashboards/bin/opensearch-dashboards --opensearch.hosts="http://$WEBHARE_OPENSEARCH_BINDHOST:$((WEBHARE_BASEPORT + 6))/" --port="$((WEBHARE_BASEPORT + 7))" &
+/usr/local/opt/opensearch-dashboards/bin/opensearch-dashboards --opensearch.hosts="http://$WEBHARE_OPENSEARCH_BINDHOST:$((WEBHARE_BASEPORT + 6))/" --host="$DASHBOARD_HOST" --port="$DASHBOARD_PORT" &
 
 trap "kill %1; wait %1" TERM EXIT
 
-OPENURL="http://127.0.0.1:$((WEBHARE_BASEPORT + 7))/app/dev_tools#/console"
+OPENURL="http://$DASHBOARD_HOST:$DASHBOARD_PORT/app/dev_tools#/console"
 
 while true; do
   if curl --silent --fail "$OPENURL" >/dev/null 2>&1 ; then
