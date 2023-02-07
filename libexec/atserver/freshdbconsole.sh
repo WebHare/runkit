41214314cd9e6aaedef300331e8f775725bfdbd4
Arnold Hendriks <arnold@webhare.nl>
2023-02-07 10:04:21 +0100

    improve freshdb cleanup

--- libexec/atserver/freshdbconsole.sh
+++ libexec/atserver/freshdbconsole.sh
@@ -10,9 +10,16 @@ if [ ! -f "$ALLOWFRESHFILE" ]; then
   die "freshdbconsole WIPES YOUR DATABASE on startup. To prove this is what you wish, please create a file named '$ALLOWFRESHFILE'"
 fi
 
-rm -rf -- "$WEBHARE_DATAROOT"/postgresql
-rm -rf -- "$WEBHARE_DATAROOT"/log "$WEBHARE_DATAROOT"/system.cache "$WEBHARE_DATAROOT"/publisher.pb "$WEBHARE_DATAROOT"/publisher.pd "$WEBHARE_DATAROOT"/system.last-shrinkwrap-var
-rm -rf -- "$WEBHARE_DATAROOT"/index "$WEBHARE_DATAROOT"/opensearch
+# Delete all whdata folders, except etc (where allow-fresh-db lives, but it should probably be in whrunkit folder) and modules
+# Prevent unneeded geoip redownloads
+rm -rf "$WEBHARE_DATAROOT"/previous-freshdb
+mkdir -p "$WEBHARE_DATAROOT"/previous-freshdb
+find "$WEBHARE_DATAROOT" -not -name previous-freshdb \
+                         -not -name etc \
+                         -not -name "installedmodules*" \
+                         -not -name geoip \
+                         -mindepth 1 -maxdepth 1 \
+                         -exec mv {} "$WEBHARE_DATAROOT"/previous-freshdb/ ";"
 
 if [ -x "$WEBHARE_DATAROOT/etc/fresh-db-setup.sh" ]; then  # TODO too many startup approaches - can WEBHARE_POSTSTARTSCRIPT solve this?
   "$WEBHARE_DATAROOT/etc/fresh-db-setup.sh" &
