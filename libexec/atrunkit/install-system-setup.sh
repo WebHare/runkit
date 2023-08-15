0461a7127b925b11a549bdc017f5130a90811a3e
Arnold Hendriks <arnold@webhare.nl>
2023-08-15 21:30:35 +0200

    ensure decent system configuration. currently called  in escrow recipes

---
+++ libexec/atrunkit/install-system-setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# short: Install system configuration script
+
+set -eo pipefail
+
+if ! hash systemctl 2>/dev/null ; then
+  echo "No systemctl - cannot install system-setup"
+  exit 1
+fi
+
+# TODO using a temp file is nicer
+cat > /etc/systemd/system/runkit-system-setup.service << HERE
+[Unit]
+Description=Automatically fix system configuration for WebHare
+
+[Service]
+Type=oneshot
+
+ExecStart="$WHRUNKIT_ROOT"/bin/runkit system-setup
+
+[Install]
+WantedBy=multi-user.target
+HERE
+
+systemctl daemon-reload
+systemctl enable runkit-system-setup #ensure autostart
+systemctl start runkit-system-setup
+
+echo "System-setup initialized!"
---
+++ libexec/atrunkit/system-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# short: Fix system configuration for WebHare
+
+if [ "$(uname)" != "Linux" ]; then
+  # perhaps we'll add a macos version in the future to at least fix launchctl ulimit?
+  echo "system-setup is only supported for linux"
+  exit 1
+fi
+
+OVERCOMMIT="$(cat /proc/sys/vm/overcommit_memory)"
+if [ "$OVERCOMMIT" == "0" ] || [ "$OVERCOMMIT" == "2" ]; then
+  echo "Fixing overcommit setting, WebHare requires 1, was: $OVERCOMMIT"
+  exit 1
+fi
