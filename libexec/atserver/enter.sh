fa4428830b5fef75885cd4768174bc805e41c77a
Arnold Hendriks <arnold@webhare.nl>
2024-04-02 12:21:52 +0200

    restore target env variables when enter-ing

--- libexec/atserver/enter.sh
+++ libexec/atserver/enter.sh
@@ -5,14 +5,18 @@
 [ -n "$WHRUNKIT_CONTAINERNAME" ] || die "Not running in a container"
 iscontainerup "$WHRUNKIT_CONTAINERNAME" || die "Container $WHRUNKIT_CONTAINERNAME is not running"
 
-# we're not using nsenter --all now, it doesn't inherit the  containers environment
+PID="$( "$WHRUNKIT_CONTAINERENGINE" inspect -f '{{.State.Pid}}' "$WHRUNKIT_CONTAINERNAME" )"
 
-if [ "$1" != "" ]; then #sending a command
-  EXECOPTS="-i"
-  CMD=("$@")
-else
-  EXECOPTS="-ti"
-  CMD=("/bin/bash")
+# Note: future versions of nsenter will learn '--env' which will remove the need for this (it was added nov 2023)
+# Import the target environment. You shouldn't target untrusted containers with this code!
+while IFS= read -r LINE
+do
+  # shellcheck disable=SC2163 disable=SC2086
+  export $LINE
+done < <(xargs -0 -L1 -a "/proc/$PID/environ" | grep -E '^[A-Za-z0-9_]+=');
+
+if [ "$1" != "" ]; then
+   exec nsenter --all -t "$PID" "$@"
+ else
+  PS1="[$(hostname --short)%${WHRUNKIT_TARGETSERVER} \W]\$ " nsenter --all -t "$PID" /bin/bash --noprofile --norc
 fi
-
-exec "$WHRUNKIT_CONTAINERENGINE" exec $EXECOPTS "$WHRUNKIT_CONTAINERNAME" env WEBHARE_CLI_USER="$WEBHARE_CLI_USER" PS1="[$(hostname --short)%${WHRUNKIT_TARGETSERVER} \W]\$ " "${CMD[@]}"
