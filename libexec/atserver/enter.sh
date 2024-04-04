42a75e9478c6cce092b1c3e8b9b610572f58f4ed
Arnold Hendriks <arnold@webhare.nl>
2024-04-04 13:06:06 +0200

    ensure baseport

--- libexec/atserver/enter.sh
+++ libexec/atserver/enter.sh
@@ -7,6 +7,9 @@ iscontainerup "$WHRUNKIT_CONTAINERNAME" || die "Container $WHRUNKIT_CONTAINERNAM
 
 PID="$( "$WHRUNKIT_CONTAINERENGINE" inspect -f '{{.State.Pid}}' "$WHRUNKIT_CONTAINERNAME" )"
 
+export WEBHARE_BASEPORT=13679 #Inside a container the baseport is always 13679
+# Note: WH5.5 will explicitly set WEBHARE_BASEPORT=13679 in its container environment
+
 # Note: future versions of nsenter will learn '--env' which will remove the need for this (it was added nov 2023)
 # Import the target environment. You shouldn't target untrusted containers with this code!
 while IFS= read -r LINE
