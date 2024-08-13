186e2841e3f34e96872b37d627035cfa5fe29d39
Arnold Hendriks <arnold@webhare.nl>
2024-08-13 13:16:01 +0200

    don't touch systemd when systemctl not present

--- libexec/atrunkit/install-system.sh
+++ libexec/atrunkit/install-system.sh
@@ -4,12 +4,9 @@
 
 set -eo pipefail
 
-if ! hash systemctl 2>/dev/null ; then
-  echo "No systemctl - cannot install system-setup"
-  exit 1
-fi
+if hash systemctl 2>/dev/null ; then # check if systemctl is present, otherwise assume we're in a container or at least not responsible for system config
 
-# TODO using a temp file is nicer
+  # TODO using a temp file is nicer
 cat > /etc/systemd/system/runkit-system-setup.service << HERE
 [Unit]
 Description=Automatically fix system configuration for WebHare
@@ -23,10 +20,11 @@ ExecStart="$WHRUNKIT_ROOT"/bin/runkit system-setup
 WantedBy=multi-user.target
 HERE
 
-systemctl daemon-reload
-systemctl enable runkit-system-setup #ensure autostart
-systemctl start runkit-system-setup
+  systemctl daemon-reload
+  systemctl enable runkit-system-setup #ensure autostart
+  systemctl start runkit-system-setup
 
-echo "System-setup initialized!"
+  echo "System-setup initialized!"
+fi
 
 addpackage borgbackup openssh-client podman jq
