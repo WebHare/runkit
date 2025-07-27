2f15c1e7e5f6473b9aa8301b7dcc6aa166219f31
Arnold Hendriks <arnold@webhare.nl>
2025-07-27 22:49:25 +0200

    actually pull state

--- libexec/atrunkit/__state-report.sh
+++ libexec/atrunkit/__state-report.sh
@@ -5,6 +5,7 @@ if [ -z "$REPORTSTATEURL" ]; then
   exit 0
 fi
 
+STATE=$($WHRUNKIT_ORIGCOMMAND get-state-json)
 while read -r POSTURL; do
   echo "$STATE" | curl --silent --request POST --fail --data-binary "@-" "$POSTURL" > /dev/null
 done <<<"$REPORTSTATEURL"
